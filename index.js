7e6f67dc5fe7439ac1071f50ba4ae3e0a5cb13bc
Jacob Goh <jacobgoh101@gmail.com>
2017-11-22 16:12:21 +0800

    add status success to results

--- index.js
+++ index.js
@@ -2,32 +2,35 @@ var verifier = require("email-verify");
 var permute = require("email-permutator");
 
 module.exports = function(context, cb) {
-  var emails = permute(context.data);
-  var verificationResults = [];
-  emails.map(email => {
-    var obj = {};
-    verifier.verify(email, function(err, info) {
-      if (err) cb(err);
-      else {
-        obj = {
-          email: email,
-          success: info.success,
-          info: info
-        };
-        verificationResults.push(obj);
-      }
+    var emails = permute(context.data);
+    var verificationResults = [];
+    emails.map(email => {
+        var obj = {};
+        verifier.verify(email, function(err, info) {
+            if (err) cb(err);
+            else {
+                obj = {
+                    email: email,
+                    success: info.success,
+                    info: info
+                };
+                verificationResults.push(obj);
+            }
+        });
     });
-  });
-  setInterval(function() {
-    if (verificationResults.length == emails.length) {
-      var filterEmails = verificationResults.map(obj => {
-        if (obj.success) {
-          return obj.email;
+    setInterval(function() {
+        if (verificationResults.length == emails.length) {
+            var filterEmails = verificationResults.map(obj => {
+                if (obj.success) {
+                    return obj.email;
+                }
+                return null;
+            });
+            filterEmails = filterEmails.filter(email => email);
+            cb(null, {
+                status: 'success',
+                emails: filterEmails
+            });
         }
-        return null;
-      });
-      filterEmails = filterEmails.filter(email => email);
-      cb(null, filterEmails);
-    }
-  }, 500);
+    }, 500);
 };
